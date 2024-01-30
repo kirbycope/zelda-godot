c59782bf1832f7e7385c159ace44fcd1b8c3185a
Timothy Cope <kirbycope@gmail.com>
2024-01-29 19:26:08 -0800

    eod

--- player.gd
+++ player.gd
@@ -1,8 +1,30 @@
 extends CharacterBody2D
 
 var speed = 100
+var last_direction = Vector2.RIGHT
+var animated_sprite
+
+func _ready():
+	animated_sprite = $AnimatedSprite2D
+	add_to_group("Player")
 
 func _physics_process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * speed
+	if direction != Vector2.ZERO:
+		last_direction = direction
+	if direction.x != 0:
+		animated_sprite.play("walk_right")
+	elif direction.y < 0:
+		animated_sprite.play("walk_up")
+	elif direction.y > 0:
+		animated_sprite.play("walk_down")
+	elif direction == Vector2.ZERO:
+		if last_direction.x != 0:
+			animated_sprite.play("idle_right")
+		elif last_direction.y < 0:
+			animated_sprite.play("idle_up")
+		elif last_direction.y > 0:
+			animated_sprite.play("idle_down")
+	animated_sprite.flip_h = last_direction.x < 0
 	move_and_slide()
