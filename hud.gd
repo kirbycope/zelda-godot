956f4c2ade0d0db2446fa03ec6a1623b1b0b9388
Timothy Cope <kirbycope@gmail.com>
2024-02-06 13:12:52 -0800

    wip

---
+++ hud.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+var wooden_sword = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func show_wooden_sword():
+	$wooden_sword.visible = true
--- wooden_sword.gd
+++ wooden_sword.gd
@@ -3,5 +3,6 @@ extends Area2D
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		Global.has_wooden_sword = true
-		#$hud/wooden_sword.visible = true
+		var hud_node = $"../TileMap/player/hud"
+		hud_node.show_wooden_sword()
 		self.visible = false
